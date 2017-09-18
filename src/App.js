217fb722574b37fd7dc575692f4e8362bcfb836e
cmd-kvn <kvn.m.wong@gmail.com>
2017-09-18 16:59:44 -0700

    creates handleSubmit function and AddTodo component - handleSubmit exists in App to change the state of todos - AddTodo is used in App and has handleSubmit as a prop - AddTodo is a form that takes input and adds to the TodoList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             todos: ['hi', 'bye'],
         };
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(input) {
+        this.setState(prevState => ({todos: prevState.todos.concat(input)}));
     }
 
     render() {
@@ -19,9 +25,10 @@ class App extends Component {
                     <h2>Welcome to React</h2>
                 </div>
                 <p className="App-intro">
-                Let's make a todo list
+                    Let's make a todo list
                 </p>
-                <TodoList todos={this.state.todos}/>
+                <TodoList todos={this.state.todos} />
+                <AddTodo handleSubmit={this.handleSubmit} />
             </div>
         );
     }
@@ -34,13 +41,33 @@ const TodoList = (props) => { /* the array of todos from App state are on the pr
             {props.todos.map((todo, i) => (
                 <TodoItem key={i} todo={todo} />
             ))}
-        </ul>);
+        </ul>
+    );
 };
 
 const TodoItem = (props) => { /* the array of todos from App state passed to TodoList are on the props object */
     const { todo } = props; // so you don't have to write 'props.todo'
-    
+
     return <li>{todo}</li>;
+};
+
+const AddTodo = (props) => { /* the handleSubmit function from App is on the props object  */
+    let input;
+
+    return (
+        <div>
+            <form 
+                onSubmit={e => {
+                    e.preventDefault();
+                    if(!input.value.trim()) return;
+                    props.handleSubmit(input.value);
+                    input.value = '';
+                }}>
+                <input ref={node => {input = node;}}/>
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+    );
 };
 
 export default App;
