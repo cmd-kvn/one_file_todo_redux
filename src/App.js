144929f68729bc6d915a6d263b73ed8b51f62797
cmd-kvn <kvn.m.wong@gmail.com>
2017-09-18 14:00:12 -0700

    sets up state in App - creates TodoList component - TodoList component uses props from App state and is the component is used in App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            todos: ['hi'],
+        };
+    }
+
     render() {
         return (
             <div className="App">
@@ -11,11 +19,20 @@ class App extends Component {
                     <h2>Welcome to React</h2>
                 </div>
                 <p className="App-intro">
-				Let's make a todo list
+                Let's make a todo list
                 </p>
+                <TodoList todos={this.state.todos}/>
             </div>
         );
     }
 }
+
+const TodoList = (props) => {
+    return (
+        <ul>
+            {props.todos}
+            {/* todos go in here */}
+        </ul>);
+};
 
 export default App;
